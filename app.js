66288f9f76b632e953ab1f4d1cc4e6ecac91ae8e
juliancst <jufecas14@gmail.com>
2021-10-21 13:50:31 -0500

    se termina de dar estilos responsive

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, "./public")));
+
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
