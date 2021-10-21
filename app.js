63ec8b7d156873126075cb45ccab1c4d6de2a5eb
juliancst <jufecas14@gmail.com>
2021-10-20 23:30:53 -0500

    se agregan formularios, log in y register con estilos

--- app.js
+++ app.js
@@ -7,5 +7,13 @@ app.use(express.static(path.resolve(__dirname, "./public")));
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+
 app.listen(port, () => console.log(`Server corriendo en ${port}`));
 
